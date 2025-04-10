2acae999317f00162fed41b0cb8316c3bb03b2d7
signal <metacode10@gmail.com>
2025-04-10 17:26:35 +0900

    a

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,10 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-def hello_world(request):
-    return HttpResponse('Hello world')
-
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, "accountapp/index.html")
--- kys/urls.py
+++ kys/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from accountapp import views as main_views
+import accountapp
+
+
 urlpatterns = [
+    path('', main_views.index, name="index"),
     path('admin/', admin.site.urls),
-    path('accounts/', include('accountapp.urls')),
+    # path('accounts/', include('accountapp.urls')),
 ]
